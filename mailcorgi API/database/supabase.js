b7cadb46b2b6cc9fd6c8982a4361fea18dc5d2d4
Hugo Hu <69009840+Hugoyhu@users.noreply.github.com>
2023-06-05 21:13:41 -0400

    Update supabase.js

--- mailcorgi API/database/supabase.js
+++ mailcorgi API/database/supabase.js
@@ -14,6 +14,24 @@ module.exports = class Client {
         verification / other tools to add in 
     */
 
+    generateSearchTokenString(string) {
+        let array = string.split(" ");
+        let returnedString = "";
+        let lengthOf = array.length;
+
+        for (let i = 0; i < lengthOf; i++) {
+            if (i == lengthOf - 1) {
+                returnedString += array[i]
+            } else {
+                returnedString += array[i]
+                returnedString += " & "
+            }
+
+        }
+        console.log(returnedString);
+        return returnedString;
+    }
+
     async checkToken(email, token) {
         const tokens = async () => {
             const response = await this.client
@@ -35,6 +53,60 @@ module.exports = class Client {
         return true;
 
     }
+
+    async updateSudoAddress(email, address) {
+        const addresses = async () => {
+            const response = await this.client
+                .from('people')
+                .select('email')
+                .eq('email', email)
+
+            return response;
+        }
+
+        const response = await addresses();
+
+        if (response["data"].length == 0) {
+            console.log("updating address by insert")
+            await this.client
+                .from('people')
+                .insert(
+                    {
+                        email: email,
+                        phone: address.phone,
+                        slack: address.slack,
+                        name: address.name,
+                        club: address.club,
+                        addr1: address.addr1,
+                        addr2: address.addr2,
+                        city: address.city,
+                        state: address.state,
+                        zip: address.zip,
+                        country: address.country,
+                    }).then(console.log);
+        }
+
+        console.log("updating");
+        const { error } = await this.client
+            .from('people')
+            .update({
+                phone: address.phone,
+                slack: address.slack,
+                name: address.name,
+                club: address.club,
+                addr1: address.addr1,
+                addr2: address.addr2,
+                city: address.city,
+                state: address.state,
+                zip: address.zip,
+                country: address.country
+            })
+            .eq('email', email);
+
+        console.log(address);
+    }
+
+
 
     async updateAddress(email, token, address) {
 
@@ -128,6 +200,7 @@ module.exports = class Client {
 
         console.log(address);
     }
+
 
     async createToken(email, timeValid) {
         // create a new record
@@ -236,6 +309,7 @@ module.exports = class Client {
                 .select()
                 .textSearch('email', email);
 
+            console.log(response);
             return response;
         }
 
@@ -244,7 +318,21 @@ module.exports = class Client {
         if (addressData["data"].length == 0) {
             return {
                 error: "no email found",
-                data: []
+                data: {
+                    email: email,
+                    phone: '',
+                    slack: '',
+                    name: '',
+                    club: '',
+                    addr1: '',
+                    addr2: '',
+                    city: '',
+                    state: '',
+                    zip: '',
+                    country: '',
+                    public_wildcard: null,
+                    private_wildcard: null
+                }
             }
         }
 
@@ -278,7 +366,21 @@ module.exports = class Client {
         if (addressData["data"].length == 0) {
             return {
                 error: "no email found",
-                data: []
+                data: {
+                    email: email,
+                    phone: '',
+                    slack: '',
+                    name: '',
+                    club: '',
+                    addr1: '',
+                    addr2: '',
+                    city: '',
+                    state: '',
+                    zip: '',
+                    country: '',
+                    public_wildcard: null,
+                    private_wildcard: null
+                }
             }
         }
 
@@ -287,6 +389,91 @@ module.exports = class Client {
             error: null,
             data: addressData.data
         }
+    }
+
+    async checkSudoAddressByUID(UID) {
+        //
+        //
+        //
+        // DANGER ZONE: DO NOT USE UNLESS PROPERLY AUTHENTICATED! THERE ARE NO AUTHENTICATION CHECKS
+        // same as above, but CASE SENSITIVE and not "matching"
+        // 
+        //
+        //
+
+        const query = async () => {
+            const response = await this.client
+                .from('people')
+                .select()
+                .eq('slack', UID);
+
+            return response;
+        }
+
+        const addressData = await query();
+
+        if (addressData["data"].length == 0) {
+            return {
+                error: "no uid found",
+                data: {
+                    email: '',
+                    phone: '',
+                    slack: UID,
+                    name: '',
+                    club: '',
+                    addr1: '',
+                    addr2: '',
+                    city: '',
+                    state: '',
+                    zip: '',
+                    country: '',
+                    public_wildcard: null,
+                    private_wildcard: null
+                }
+            }
+        }
+
+
+        return {
+            error: null,
+            data: addressData.data
+        }
+    }
+
+    async checkSudoBulkByEmailName(list) {
+        var returnedData = [[], []]
+
+        const pushData = (data) => {
+            if (data.data.length > 1) {
+
+                for (var i = 0; i < data.data.length; i++) {
+                    console.log(i, "push multiple", data.data[i])
+                    returnedData[1].push(data.data[i]);
+                }
+            } else {
+                if (data.error == null) {
+                    console.log("push single", data.data[0])
+                    returnedData[0].push(data.data[0]);
+                } else {
+                    console.log("push none", data)
+                    returnedData[1].push(data.data);
+                }
+            }
+        }
+
+        for (var i = 0; i < list.length; i++) {
+
+            if (list[i][0] != '') {
+                await this.checkSudoAddressByEmailStrict(list[i][0]).then(pushData);
+            } else if (list[i][1] != '') {
+                await this.checkSudoAddressByUID(list[i][1]).then(pushData);
+            } else if (list[i][2] != '') {
+                //no email provided, check with name
+                await this.checkSudoAddressByName(list[i][2]).then(pushData);
+            }
+        }
+
+        return returnedData;
     }
 
     async checkSudoAddressByName(name) {
@@ -302,17 +489,34 @@ module.exports = class Client {
             const response = await this.client
                 .from('people')
                 .select()
-                .textSearch('name', name)
+                .textSearch('name', this.generateSearchTokenString(name))
 
             return response;
         }
 
+
         const addressData = await query();
+
+        console.log("hello", addressData);
 
         if (addressData["data"].length == 0) {
             return {
                 error: "no name found",
-                data: []
+                data: {
+                    email: '',
+                    phone: '',
+                    slack: '',
+                    name: name,
+                    club: '',
+                    addr1: '',
+                    addr2: '',
+                    city: '',
+                    state: '',
+                    zip: '',
+                    country: '',
+                    public_wildcard: null,
+                    private_wildcard: null
+                }
             }
         }
 
@@ -321,6 +525,114 @@ module.exports = class Client {
             error: null,
             data: addressData.data
         }
+
+
     }
 
+    async checkNodemaster(nodemaster) {
+        const uid = async () => {
+            const response = await this.client
+                .from('nodemasters')
+                .select()
+                .eq('slack', nodemaster)
+
+            return response;
+        }
+
+        const response = await uid();
+
+        if (response["data"].length == 0) {
+            return false;
+        }
+
+        return true;
+
+    }
+
+
+    async updateSudoAddressUID(UID, address) {
+
+        /*
+        insert or update, depending on availability of an existing record 
+
+        address should be:
+        
+        {
+            phone,
+            slack,
+            name,
+            club,
+            addr1,
+            addr2,
+            city,
+            state,
+            zip,
+            country,
+            public_wildcard,
+            private_wildcard
+        }
+
+        */
+
+
+        const UIDaddresses = async () => {
+            const response = await this.client
+                .from('people')
+                .select('slack')
+                .eq('slack', UID)
+
+            return response;
+        }
+
+        const UIDresponse = await UIDaddresses();
+
+        const Emailaddresses = async () => {
+            const response = await this.client
+                .from('people')
+                .select('slack')
+                .eq('slack', UID)
+
+            return response;
+        }
+
+        const Emailresponse = await Emailaddresses();
+
+        if (UIDresponse["data"].length == 0 || Emailresponse["data"].length == 0) {
+            await this.client
+                .from('people')
+                .insert(
+                    {
+                        email: address.email,
+                        phone: address.phone,
+                        slack: address.slack,
+                        name: address.name,
+                        club: address.club,
+                        addr1: address.addr1,
+                        addr2: address.addr2,
+                        city: address.city,
+                        state: address.state,
+                        zip: address.zip,
+                        country: address.country
+                    }).then(console.log);
+        }
+
+        const { error } = await this.client
+            .from('people')
+            .update({
+                email: address.email,
+                phone: address.phone,
+                slack: address.slack,
+                name: address.name,
+                club: address.club,
+                addr1: address.addr1,
+                addr2: address.addr2,
+                city: address.city,
+                state: address.state,
+                zip: address.zip,
+                country: address.country,
+            })
+            .eq('slack', UID);
+
+        console.log(address, "update", error);
+    }
 }
