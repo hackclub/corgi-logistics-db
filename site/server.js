037927ca975fcb691b647c94dde2b752b6a502f0
Hugo Hu <69009840+Hugoyhu@users.noreply.github.com>
2023-06-05 21:06:08 -0400

    Update server.js

--- site/server.js
+++ site/server.js
@@ -18,6 +18,28 @@ const Client = require("../mailcorgi API/database/supabase");
 const { time } = require('console');
 
 const client = new Client(process.env.SUPABASE_URL, process.env.SUPABASE_KEY, 900000);
+const supabaseClient = client;
+
+const { WebClient, LogLevel } = require("@slack/web-api");
+
+const slackClient = new WebClient(process.env.SLACK_BOT_TOKEN, {
+    // LogLevel can be imported and used to make debugging simpler
+    logLevel: LogLevel.DEBUG
+});
+
+const { App } = require('@slack/bolt');
+
+const slackApp = new App({
+    token: process.env.SLACK_BOT_TOKEN,
+    signingSecret: process.env.SLACK_SIGNING_SECRET,
+    socketMode: true, // add this
+    appToken: process.env.SLACK_APP_TOKEN // add this
+});
+
+
+const modalViews = require('../slack/modals');
+const modals = require('../slack/modals');
+
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -181,6 +203,14 @@ app.get('/token/:token/email/:email', (req, res) => {
 
         if (status == true) {
             const sendAddress = (address) => {
+
+                try {
+                    address.data.forEach(function () { });
+                } catch (error) {
+                    res.render('../site/views/noResults', {
+                    });
+                    return
+                }
                 res.render('../site/views/viewAddress', {
                     error: address.error,
                     address: address.data,
@@ -214,6 +244,8 @@ app.get('/token/:token/edit/:email', (req, res) => {
                     error: address.error,
                     address: address.data[0],
                     query: req.params.email,
+                    url: process.env.SITE_URL,
+                    token: req.params.token
                 });
 
             }
@@ -264,6 +296,16 @@ app.get('/token/:token/name/:name', (req, res) => {
 
         if (status == true) {
             const sendAddress = (address) => {
+
+                try {
+                    address.data.forEach(function () { });
+                } catch (error) {
+                    console.log(error, "\n\n\n test \n\n\n")
+                    res.render('../site/views/noResults', {
+                    });
+                    return
+                }
+
                 res.render('../site/views/viewAddress', {
                     error: address.error,
                     address: address.data,
@@ -304,5 +346,304 @@ app.get('/token/:token/add', (req, res) => {
     //     emailSite: "localhost:8080/token/" + process.env.
     // });
 });
+
+
+
+
+
+
+
+
+slackApp.command('/update', async ({ ack, body, command, client, logger }) => {
+    // Acknowledge command request
+    await ack();
+
+
+    slackProfileInfo = await slackClient.users.info({ user: command.user_id });
+    console.log(slackProfileInfo);
+    address = await supabaseClient.checkSudoAddressByUID(command.user_id);
+    console.log(address);
+
+    if (address.error === null || address.error === undefined) {
+        if (address.data[0].phone === null || address.data[0].phone === undefined) {
+            address.data[0].phone = '';
+        }
+
+        if (address.data[0].addr2 === null || address.data[0].addr2 === undefined) {
+            address.data[0].addr2 = '';
+        }
+
+        addressView = modalViews.generateAddressModal(address.data[0].email, address.data[0]);
+    } else {
+        addressView = modalViews.generateAddressModal(slackProfileInfo.user.profile.email, address.data);
+    }
+
+    console.log(addressView)
+
+
+    try {
+        // Call views.open with the built-in client
+        const result = await client.views.open({
+            // Pass a valid trigger_id within 3 seconds of receiving it
+            trigger_id: body.trigger_id,
+            // View payload
+            view: addressView
+        });
+    }
+    catch (error) {
+        logger.error(error);
+        console.log("\n\n\n", error.data.response_metadata);
+
+    }
+});
+
+// slackApp.view('region_select', async ({ ack, view, body }) => {
+//     await ack({
+//         response_action: 'update',
+//         view: modalViews.generateCountryModal(view['state']['values']['static_select_region']['static_select-action']['selected_option']['value']),
+//     });
+// });
+
+slackApp.view('address-modal', async ({ ack, body, view, client, logger }) => {
+    await ack();
+    console.log("\n\n\n", view['state']['values']);
+    slackUID = body.user.id;
+
+    address = view['state']['values'];
+
+    supabaseClient.updateSudoAddressUID(slackUID, {
+        email: address.email.value.value,
+        slack: slackUID,
+        phone: address.phone.value.value,
+        name: address.name.value.value,
+        addr1: address.addr1.value.value,
+        addr2: address.addr2.value.value,
+        city: address.city.value.value,
+        state: address.state.value.value,
+        zip: address.zip.value.value,
+        country: address.country.value.value
+    });
+
+});
+
+slackApp.command('/nodemaster', async ({ ack, body, command, client, logger, say }) => {
+    // Acknowledge command request
+    await ack();
+
+    try {
+        slackID = body.text.split("<@")[1].split("|")[0];
+    } catch {
+        try {
+
+            // Call the chat.postEphemeral method using the WebClient
+            const result = await client.chat.postEphemeral({
+                channel: body.channel_id,
+                user: body.user_id,
+                text: "Enter only a slack tag."
+            });
+
+            console.log(result);
+        }
+        catch (error) {
+            console.error(error);
+        }
+    }
+
+    requestID = body.user_id;
+
+    nodemaster = await supabaseClient.checkNodemaster(requestID);
+
+    if (nodemaster) {
+
+        slackProfileInfo = await slackClient.users.info({ user: slackID });
+        console.log(slackProfileInfo);
+        address = await supabaseClient.checkSudoAddressByUID(slackID);
+        console.log(address);
+
+        if (address.error != null) {
+            try {
+                const result = await client.chat.postEphemeral({
+                    channel: body.channel_id,
+                    user: body.user_id,
+                    text: `
+No address for ${address.name}.
+                `
+                });
+            } catch (error) {
+                logger.error(error);
+            }
+        }
+        else {
+            try {
+                // Call views.open with the built-in client
+                if (address.data[0].addr2 == null || address.data[0].addr2 == '') {
+                    const result = await client.chat.postEphemeral({
+                        channel: body.channel_id,
+                        user: body.user_id,
+                        text: `
+Address for ${address.data[0].name}
+${address.data[0].phone}, ${address.data[0].email}, ${address.data[0].slack}
+\`\`\`
+${address.data[0].addr1}
+${address.data[0].city} ${address.data[0].state}  ${address.data[0].zip}
+${address.data[0].country}
+\`\`\`
+                    `
+                    });
+                }
+                else {
+                    const result = await client.chat.postEphemeral({
+                        channel: body.channel_id,
+                        user: body.user_id,
+                        text: `
+Address for ${address.data[0].name}
+${address.data[0].phone}, ${address.data[0].email}, ${address.data[0].slack}
+    
+\`\`\`
+${address.data[0].addr1}
+${address.data[0].addr2}
+${address.data[0].city} ${address.data[0].state}  ${address.data[0].zip}
+${address.data[0].country}
+\`\`\`
+                    `
+                    });
+                }
+            }
+            catch (error) {
+                logger.error(error);
+            }
+        }
+
+    } else {
+        try {
+
+            // Call the chat.postEphemeral method using the WebClient
+            const result = await client.chat.postEphemeral({
+                channel: body.channel_id,
+                user: body.user_id,
+                text: "You're not a nodemaster! What a silly goose"
+            });
+
+            console.log(result);
+        }
+        catch (error) {
+            console.error(error);
+        }
+    }
+
+    console.log(slackID, requestID);
+});
+
+
+
+slackApp.command('/nodemasteredit', async ({ ack, body, command, client, logger, say }) => {
+    // Acknowledge command request
+    await ack();
+
+    try {
+        slackID = body.text.split("<@")[1].split("|")[0];
+    } catch {
+        try {
+
+            // Call the chat.postEphemeral method using the WebClient
+            const result = await client.chat.postEphemeral({
+                channel: body.channel_id,
+                user: body.user_id,
+                text: "Enter only a slack tag."
+            });
+
+            console.log(result);
+        }
+        catch (error) {
+            console.error(error);
+        }
+    }
+
+    requestID = body.user_id;
+
+    nodemaster = await supabaseClient.checkNodemaster(requestID);
+
+    if (nodemaster) {
+
+        slackProfileInfo = await slackClient.users.info({ user: slackID });
+        console.log(slackProfileInfo);
+        address = await supabaseClient.checkSudoAddressByUID(slackID);
+        console.log(address);
+
+        if (address.error != null) {
+            addressView = modalViews.generateEditAddressModal(slackProfileInfo.user.profile.email, slackID, address.data);
+        }
+        else {
+            if (address.data[0].phone === null || address.data[0].phone === undefined) {
+                address.data[0].phone = '';
+            }
+
+            if (address.data[0].addr2 === null || address.data[0].addr2 === undefined) {
+                address.data[0].addr2 = '';
+            }
+            addressView = modalViews.generateEditAddressModal(address.data[0].email, slackID, address.data[0]);
+        }
+        try {
+            // Call views.open with the built-in client
+            const result = await client.views.open({
+                // Pass a valid trigger_id within 3 seconds of receiving it
+                trigger_id: body.trigger_id,
+                // View payload
+                view: addressView
+            });
+        }
+        catch (error) {
+            logger.error(error);
+        }
+    } else {
+        try {
+
+            // Call the chat.postEphemeral method using the WebClient
+            const result = await client.chat.postEphemeral({
+                channel: body.channel_id,
+                user: body.user_id,
+                text: "You're not a nodemaster! What a silly goose"
+            });
+
+            console.log(result);
+        }
+        catch (error) {
+            console.error(error);
+        }
+    }
+
+    console.log(slackID, requestID);
+});
+
+slackApp.view('edit-modal', async ({ ack, body, view, client, logger }) => {
+    await ack();
+
+    address = view['state']['values'];
+
+    slackUID = address.uid.value.value;
+
+    console.log(slackUID);
+
+    supabaseClient.updateSudoAddressUID(slackUID, {
+        email: address.email.value.value,
+        slack: slackUID,
+        phone: address.phone.value.value,
+        name: address.name.value.value,
+        addr1: address.addr1.value.value,
+        addr2: address.addr2.value.value,
+        city: address.city.value.value,
+        state: address.state.value.value,
+        zip: address.zip.value.value,
+        country: address.country.value.value
+    });
+
+});
+
 app.listen(process.env.PORT || 8080,
     () => console.log("Server is running..."));
+
+(async () => {
+    await slackApp.start(process.env.PORT || 3000);
+
+    console.log('⚡️ Bolt app is running!');
+})();
